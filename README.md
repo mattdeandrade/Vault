479363ce3c04ed4fb96cea6783de4713d90e94e6
Matthew De Andrade <fayrfax@gmail.com>
2024-09-05 07:59:58 -0400

    final

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Vault
+
+//pseudo code: assign each number in the combination
+//an expression that evaulates into the appropriate
+//value. Be sure to use each operator "+, -, _, /" at
+//least once.
+//first number = 40 - 10 - 10 - 10 (saves 10 to first)
+//second number = 10 + 10 + 10 + 10 (saves 40 to second)
+//third number = 10 / 10 _ 39 (saves 39 to third)
+//save the required string to "message"
+//use the alert to print message + vault combo, and
+//remember to include spacing.
---
+++ vault.js
@@ -0,0 +1,16 @@
+const first=40-10-10-10;
+const second=10+10+10+10;
+const third=10/10*39;
+const message="You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+alert(message + first + " " + second+ " " + third);
+
+//pseudo code: assign each number in the combination
+//an expression that evaulates into the appropriate
+//value. Be sure to use each operator "+, -, *, /" at
+//least once.
+//first number = 40 - 10 - 10 - 10 (saves 10 to first)
+//second number = 10 + 10 + 10 + 10 (saves 40 to second)
+//third number = 10 / 10 * 39 (saves 39 to third)
+//save the required string to "message"
+//use the alert to print message + vault combo, and
+//remember to include spacing.
